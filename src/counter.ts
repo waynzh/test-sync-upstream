ee581852f41d134d2a1444eff4342938d90e1f09
Wayne Zhang <zhanghaowei19W@gmail.com>
2023-03-25 23:02:08 +0800

    1st change

--- src/counter.ts
+++ src/counter.ts
@@ -2,7 +2,7 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `update one more time`
+    element.innerHTML = `1st`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
