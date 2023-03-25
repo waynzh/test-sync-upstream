71051b021f47230946e56cd55bdbd73e8820fb2a
Wayne Zhang <zhanghaowei19W@gmail.com>
2023-03-25 16:38:25 +0800

    update

--- src/counter.ts
+++ src/counter.ts
@@ -2,8 +2,9 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `count is equal ${counter}`
+    element.innerHTML = `update once`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
+  console.log(1);
 }
