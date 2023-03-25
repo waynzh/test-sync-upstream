c0ef6577a912e010b225445d674a558764ed3b8e
Wayne Zhang <zhanghaowei19W@gmail.com>
2023-03-25 23:02:25 +0800

    2nd change

--- src/counter.ts
+++ src/counter.ts
@@ -2,10 +2,11 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `1st`
+    element.innerHTML = `1st 2nd`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
   console.log(1);
   console.log("more commits");
+  console.log("2nd");
 }
